73846a8773982e1687928e8e525ac1b2f029856b
Danielle Levinson <dclevinson@gmail.com>
2013-12-02 09:08:53 -0800

    corrected names of functions and variables

--- independentSetTrees/independent_set_tree.py
+++ independentSetTrees/independent_set_tree.py
@@ -25,22 +25,22 @@ class Tree:
 
  
 class Node:
-  memoized_disjoint_set = {}
+  memoized_independent_set = {}
   
   def __init__(self, name, value):
     self.name = name
     self.value = value
     self.children = []
 
-  def disjoint_set(self, must_include):
-    if (Node.memoized_disjoint_set.has_key(self.name)):
-      return Node.memoized_disjoint_set[self.name]
+  def independent_set(self, must_include):
+    if (Node.memoized_independent_set.has_key(self.name)):
+      return Node.memoized_independent_set[self.name]
    
     me_invitees = [] 
     me_and_grandchildren = self.value
     for child in self.children:
       for grandchild in child.children:
-        info = grandchild.disjoint_set(False) 
+        info = grandchild.independent_set(False) 
         me_and_grandchildren += info['max']
         for person in info['invitees']:
           me_invitees.append(person) 
@@ -49,18 +49,16 @@ class Node:
     children_not_me = 0
     if (not must_include):
       for child in self.children:
-        info = child.disjoint_set(False)
+        info = child.independent_set(False)
         children_not_me += info['max']
         for person in info['invitees']:
           not_me_invitees.append(person)
   
     if me_and_grandchildren > children_not_me or must_include:
       me_invitees.append(self.name)
-      print "appending " + self.name + " to invitees"
-      print "me_and_grandchildren is ", me_and_grandchildren
       return_hash = {'max': me_and_grandchildren, 'invitees': me_invitees}
     else:
       return_hash = {'max': children_not_me, 'invitees': not_me_invitees}
     
-    Node.memoized_disjoint_set[self.name] = return_hash
+    Node.memoized_independent_set[self.name] = return_hash
     return return_hash
--- independentSetTrees/solver.py
+++ independentSetTrees/solver.py
@@ -6,10 +6,10 @@ my_tree = independent_set_tree.Tree(my_file)
 boss_choice = raw_input("Do you require that the CEO be invited to the party? (Y/N)")
 
 if(boss_choice == "Y" or boss_choice == "y"):
-  answer = my_tree.root.disjoint_set(True)
+  answer = my_tree.root.independent_set(True)
 
 else:
-  answer = my_tree.root.disjoint_set(False)
+  answer = my_tree.root.independent_set(False)
 
 
 print ""
